849bb77508d0b918f62266b3a90595dd162668e9
MSilva505 <ftsoffline606@gmail.com>
2025-02-21 18:58:51 -0400

    um desafio proposto pelo curso

---
+++ desafiodofelipao.js
@@ -0,0 +1,29 @@
+let xpDoHeroi = 30000.00
+let nomeDoHeroi = "Marcelo carioca"
+if(xpDoHeroi<=1000.0){
+    console.log("XP do heroi esta no nivel bronze")
+}
+else if(xpDoHeroi >=1001.0 && xpDoHeroi <=2000.0){
+    console.log("XP do heroi esta no nivel ferro")
+}
+else if(xpDoHeroi >=2001.0 && xpDoHeroi <=5000.0){
+    console.log("XP do heroi esta no nivel Plata")
+}
+else if(xpDoHeroi >=5001.0 && xpDoHeroi <=6000.0){
+    console.log("XP do heroi esta no nivel Ouro")
+}
+else if(xpDoHeroi >=6001.0 && xpDoHeroi <=7000.0){
+    console.log("XP do heroi esta no nivel Platina de Diamante")
+}
+else if(xpDoHeroi >=7001.0 && xpDoHeroi <=8000.0){
+    console.log("XP do heroi esta no nivel Ascendente")
+}
+else if(xpDoHeroi >=8001.0 && xpDoHeroi <=9000.0){
+    console.log("XP do heroi esta no nivel Imortal")
+}
+else if(xpDoHeroi >= 9001.0){
+    console.log("XP do heroi esta no nivel Radiante")
+}
+
+console.log(" O Heroi "+nomeDoHeroi+" e esta no nivel de "+xpDoHeroi)
+ 
